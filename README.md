083881dd45511909efff9405862c310c188ac25b
Dan McKeon <dpmckeon@gmail.com>
2017-02-26 15:38:54 -0800

    Create loop for user filter input and begin to populate readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# go-to-recipes
+# go-to-recipes app
+## Dan McKeon
+
+# Description
+
+We recently learned how Ruby can interact with SQL databases and this app is meant to be an exploration of this functionality. I put a few of my favorite recipes on here and the app is meant to easily create a grocery list for each recipe. Users are able to access database items.
+
+...EDIT?
+...ADD?
+...DELETE?
+
+# Instructions
+
+Ensure you have sqlite3 gem installed to run this application
+First run ruby seeds.rb to seed the database
+Then run ruby recipes.rb to access user interface
--- recipes.rb
+++ recipes.rb
@@ -10,12 +10,34 @@ require 'sqlite3'
 recipes_db = SQLite3::Database.new("recipes.db")
 recipes_db.results_as_hash = true
 
-puts "Please choose a metric to filter recipes by: "
-puts "t: type (entree, dessert, etc.)
-w: website/blog
-v: vegan meals only
-c: cook time (you will enter max cook time next)
-n: no filter"
+valid_filter = false
 
+while valid_filter == false
 
-puts(recipes_db.execute("SELECT * FROM recipes"))
+	puts "Please choose a metric to filter recipes by: " 
+	puts "t: type (entree, dessert, etc.)
+	s: source (website/blog)
+	v: vegan meals only
+	c: cook time (you will enter max cook time next)
+	n: no filter"
+
+	user_filter = gets.chomp
+
+	if user_filter == "t" || user_filter == "T"
+		valid_filter = true
+	elsif user_filter == "s" || user_filter == "S"
+		valid_filter = true
+	elsif user_filter == "v" || user_filter == "V"
+		valid_filter = true
+	elsif user_filter == "c" || user_filter == "C"
+		valid_filter = true
+	elsif user_filter == "n" || user_filter == "N"
+		valid_filter = true
+		recipes_db.execute("SELECT * FROM recipes") do |recipe|
+			puts "#{recipe["id"]}: #{recipe["name"]}"
+		end
+	else
+		invalid_filter_input = false
+		puts "Please ensure input is one of the following: t, s, v, c ,n"
+	end
+end
